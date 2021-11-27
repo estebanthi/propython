e2c7ab07820ef4105954861464882f6bf7784e3c
estebanthi <77675611+estebanthi@users.noreply.github.com>
2021-11-27 15:20:18 +0100

    Update index.js

--- app/pages/index.js
+++ app/pages/index.js
@@ -8,7 +8,7 @@ export default function Home({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
-        <title>Create Next App</title>
+        <title>ProPython</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
         <FeaturedPosts />
