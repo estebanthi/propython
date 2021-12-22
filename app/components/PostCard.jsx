623fcea43ec6080bf0fbc87c4229234af04ea72f
estebanthi <77675611+estebanthi@users.noreply.github.com>
2021-12-22 19:02:09 +0100

    Update PostCard.jsx

--- app/components/PostCard.jsx
+++ app/components/PostCard.jsx
@@ -56,22 +56,19 @@ const PostCard = ({post}) => {
             <p className="text-center text-lg text-gray-700 font-normal px-4 lg:px-20 mb-8">{post.excerpt}</p>
             <div className="text-center">
                 <Link href={"/post/"+post.slug}>
-                      <span className="transition duration-500 transform hover:-translate-y-1 inline-block bg-pink-600 text-lg font-medium rounded-full text-white px-8 py-3 cursor-pointer">
+                      <span onClick={() => {
+                          analytics.track({
+                              anonymousId: '48d213bb-95c3-4f8d-af97-86b2b404dcfe',
+                              event: 'Post viewed',
+                              properties: {
+                                  post: post.slug
+                              }
+                          })
+                      }} className="transition duration-500 transform hover:-translate-y-1 inline-block bg-pink-600 text-lg font-medium rounded-full text-white px-8 py-3 cursor-pointer">
                           Voir la suite
                       </span>
                 </Link>
             </div>
-            <button onClick={() => {
-                analytics.track({
-                    anonymousId: '48d213bb-95c3-4f8d-af97-86b2b404dcfe',
-                    event: 'Post viewed',
-                    properties: {
-                        post: post.slug
-                    }
-                })
-            }}>
-                click
-            </button>
         </div>
     )
 }
