34d1d13aea99af2e303ee0e6bc74c40b76e4941d
estebanthi <77675611+estebanthi@users.noreply.github.com>
2022-01-02 19:46:35 +0100

    Update [...nextauth].js

--- app/pages/api/auth/[...nextauth].js
+++ app/pages/api/auth/[...nextauth].js
@@ -60,12 +60,6 @@ export default NextAuth({
             }
             return session
         },
-        redirect({ url, baseUrl   }) {
-            console.log(url, baseUrl)
-            if (url.startsWith(baseUrl)) return url.replace(baseUrl, process.env.BASE_URL)
-            // Allows relative callback URLs
-            else if (url.startsWith("/")) return new URL(url, process.env.BASE_URL).toString()
-            return baseUrl
-        }
+   
     }
 })
