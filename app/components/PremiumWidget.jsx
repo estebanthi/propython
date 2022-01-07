cb91960837c80ebbe949e0f7508dbd02498dedee
estebanthi <77675611+estebanthi@users.noreply.github.com>
2022-01-07 19:45:28 +0100

    Update PremiumWidget.jsx

--- app/components/PremiumWidget.jsx
+++ app/components/PremiumWidget.jsx
@@ -7,7 +7,7 @@ const PremiumWidget = () => {
 
     return (
         <div className="bg-white rounded-lg shadow-lg p-8 pb-12 mb-8">
-            <h3 className="text-xl mb-8 font-semibold border-b pb-4 border-pink-600">Premium - 3.99€/mois</h3>
+            <h1 className="text-xl mb-8 font-semibold border-b pb-4 border-pink-600">Premium - 3.99€/mois</h1>
             <div>
                 L'accès à <span className="text-pink-600">des articles inédits</span>, à <span className="text-pink-600">une multitude de ressources</span>, à <span className="text-pink-600">de nouveaux projets</span>, découvrez ici pourquoi passer premium.
             </div>
