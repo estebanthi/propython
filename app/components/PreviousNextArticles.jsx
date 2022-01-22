c415a196491d9ff54e6dcbed4dbf8ea237e55ad2
estebanthi <77675611+estebanthi@users.noreply.github.com>
2022-01-22 01:46:05 +0100

    Update PreviousNextArticles.jsx

--- app/components/PreviousNextArticles.jsx
+++ app/components/PreviousNextArticles.jsx
@@ -2,12 +2,15 @@ import {useEffect, useState} from "react";
 import {getPreviousNextPosts} from "../services";
 import {ArrowBackIos, ArrowForwardIos} from "@material-ui/icons";
 import Link from "next/link"
+import {useRouter} from "next/router";
 
 
 const PreviousNextArticles = ({slug}) => {
 
     const [previous, setPrevious] = useState()
     const [next, setNext] = useState()
+
+    const router = useRouter()
 
     useEffect(async () => {
 
@@ -23,10 +26,10 @@ const PreviousNextArticles = ({slug}) => {
     return (
         <div className="flex justify-between my-12">
             <div>
-                {previous && <Link href={"/post/"+previous.slug}><div className="bg-white p-4 rounded-lg w-52 flex items-center font-semibold cursor-pointer">{previous.title}{<ArrowBackIos />}</div></Link>}
+                {previous && <div className="bg-white p-4 rounded-lg w-52 flex items-center font-semibold cursor-pointer" onClick={() => window.location = "/post/"+previous.slug}>{<ArrowBackIos />}{next.title}</div>}
             </div>
             <div>
-                {next && <Link href={"/post/"+next.slug}><div className="bg-white p-4 rounded-lg w-52 flex items-center font-semibold cursor-pointer">{next.title}{<ArrowForwardIos />}</div></Link>}
+                {next && <div className="bg-white p-4 rounded-lg w-52 flex items-center font-semibold cursor-pointer" onClick={() => window.location = "/post/"+next.slug}>{next.title}{<ArrowForwardIos />}</div>}
             </div>
         </div>
     )
